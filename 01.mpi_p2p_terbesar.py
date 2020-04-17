ed3a1adc9dbd78fd09135437ab40c83215c96d82
umarfirja16 <44198078+umarfirja16@users.noreply.github.com>
2020-04-17 10:46:22 +0700

    Fix 01.mpi_p2p_terbesar.py

--- 01.mpi_p2p_terbesar.py
+++ 01.mpi_p2p_terbesar.py
@@ -1,19 +1,23 @@
 # import mpi4py
-
+from mpi4py import MPI
 
 # buat COMM
-
+comm = MPI.COMM_WORLD
 
 # dapatkan rank proses
-
+rank = comm.Get_rank()
 
 # dapatkan total proses berjalan
-
+total = comm.Get_size()
 
 # jika saya rank terbesar maka saya akan mengirimkan pesan ke proses yang mempunyai rank 0 s.d rank terbesar-1
-if rank == :
-	
-
+if rank == total-1:
+	pesan = "pesan"
+	for i in range(0, size-1):
+		comm.send(data, dest=i)
+		print('Mengirim pesan : '.format(rank), pesan,' ke ',i)
+		
 # jika saya bukan rank terbesar maka saya akan menerima pesan yang berasal dari proses dengan rank terbesar
 else:
-	
+	pesan = comm.recv(source = total-1)
+	print('Menerima pesan : '.format(rank), pesan)
