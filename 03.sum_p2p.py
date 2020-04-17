f20cb5feed4ddd863054bc6d91704422f30f6ef3
muthianovisy <63630343+muthianovisy@users.noreply.github.com>
2020-04-17 10:22:04 +0700

    Update 03.sum_p2p.py

--- 03.sum_p2p.py
+++ 03.sum_p2p.py
@@ -1,26 +1,32 @@
 # import mpi4py
-
+from mpi4py import MPI
 
 # import library random untuk generate angka integer secara random
-
+import random
 
 # buat COMM
-
+comm = MPI.COMM_WORLD
 
 # dapatkan rank proses
-
+rank = comm.Get_rank()
 
 # dapatkan total proses berjalan
-
+size = comm.Get_size()
 
 # generate angka integer secara random untuk setiap proses
-
+randomangka = random.radint(1,5)
 
 # jika saya adalah proses dengan rank 0 maka:
 # saya menerima nilai dari proses 1 s.d proses dengan rank terbesar
 # menjumlah semua nilai yang didapat (termasuk nilai proses saya)
-if rank == :
-	
+if rank == 0:
+    sum = 0
+    for i in range(1,size):
+        data = comm.recv(source=i, tag=rank)
+        print(data)
+        sum += data['send']
+    print("Sum - ", sum)
 # jika bukan proses dengan rank 0, saya akan mengirimkan nilai proses saya ke proses dengan rank=0
 else:
-	
+    data = {'rank': rank, 'dest': 0, 'send': randomangka}
+    comm.send(data, dest=0, tag=0)
