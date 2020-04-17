82b334cb9d17924dc97c55f3bb6032b334ce23f9
umarfirja16 <44198078+umarfirja16@users.noreply.github.com>
2020-04-17 11:39:35 +0700

    Fix 02.bcast_mpi.py

--- 02.bcast_mpi.py
+++ 02.bcast_mpi.py
@@ -1,19 +1,22 @@
 # import mpi4py
-
+from mpi4py import MPI
 
 # buat COMM
-
+comm = MPI.COMM_WORLD
 
 # dapatkan rank proses
-
+rank = comm.Get_rank()
 
 # dapatkan total proses berjalan
-
+total = comm.Get_size()
 
 # jika saya rank 0 maka saya akan melakukan broadscast
-if rank == :
+if rank == 0:
+	pesan = 'pesan'
 
-	
 # jika saya bukan rank 0 maka saya menerima pesan
 else:
-	
+	pesan = None
+
+data = comm.bcast(data, root=0)
+print("rank : ",rank, "Pesan : ",pesan)
